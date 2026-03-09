769cfae449e869452268a66cf10e5a37b3958979
Nuno Cunha <nuno@exemplo.com>
2026-03-09 22:07:32 +0000

    Prepare runtime for pallet integration

--- blockchain-core/pallets/reputation/src/lib.rs
+++ blockchain-core/pallets/reputation/src/lib.rs
@@ -1,51 +1,12 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-pub use pallet::*;
+/// Moral Money Runtime
+///
+/// This runtime will host the pallets that implement the
+/// Moral Money institutional system.
+///
+/// Current pallets planned:
+///
+/// - reputation (implements reputation-model.md)
 
-#[frame_support::pallet]
-pub mod pallet {
-
-use frame_support::pallet_prelude::*;
-use frame_system::pallet_prelude::*;
-
-#[pallet::pallet]
-pub struct Pallet<T>(_);
-
-#[pallet::config]
-pub trait Config: frame_system::Config {}
-
-#[pallet::storage]
-#[pallet::getter(fn reputation)]
-
-pub type Reputation<T: Config> =
-StorageMap<
-_,
-Blake2_128Concat,
-T::AccountId,
-u32,
-ValueQuery
->;
-
-#[pallet::call]
-impl<T: Config> Pallet<T> {
-
-#[pallet::weight(0)]
-
-pub fn increase_reputation(
-origin: OriginFor<T>,
-amount: u32
-) -> DispatchResult {
-
-let who = ensure_signed(origin)?;
-
-Reputation::<T>::mutate(&who, |rep| {
-*rep += amount;
-});
-
-Ok(())
-
-}
-
-}
-
-}
+pub fn runtime_placeholder() {}
